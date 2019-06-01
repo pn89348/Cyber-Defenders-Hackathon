5c92ac0286817588e1a70bb2d20dcb3d650284fe
Sampann Nigam <sampann_nigam@yahoo.com>
2019-06-01 15:57:54 -0700

    xx

---
+++ main.py
@@ -0,0 +1,12 @@
+from scapy.all import *
+
+# rdpcap comes from scapy and loads in our pcap file
+packets = rdpcap('example.pcap')
+
+# Let's iterate through every packet
+for packet in packets:
+    # We're only interested packets with a DNS Round Robin layer
+    if packet.haslayer(DNSRR):
+        # If the an(swer) is a DNSRR, print the name it replied with.
+        if isinstance(packet.an, DNSRR):
+            print(packet.an.rrname)
